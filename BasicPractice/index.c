6c3edafc6b92ef1e0982429b95a6f82b53f55364
0xdevabir <devabir.me@gmail.com>
2025-09-17 17:11:39 +0600

    dn

---
+++ BasicPractice/index.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main(){
+
+
+
+printf("Hello, World!");
+
+}
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/0xdevabir/C-Programming.git
+// git push -u origin main
