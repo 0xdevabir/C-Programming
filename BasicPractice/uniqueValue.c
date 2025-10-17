785e3a672d4cb9deed0f3b16bb9a80cd61840e53
0xdevabir <devabir.me@gmail.com>
2025-10-17 11:21:29 +0600

    n

--- BasicPractice/uniqueValue.c
+++ BasicPractice/uniqueValue.c
@@ -2,14 +2,24 @@
 int main()
 {
 
-    int N;
+    int N , equal, araEqual=0;
     int ara[200000];
     
-    scanf("%d", &N);
+    scanf(" %d", &N);
 
 
+    for(int i=1;i<=N;i++){
+        scanf("%d", &ara[i]);
+    }
+    equal=ara[1];
+    for(int i=1;i<=N;i++){
+        if(equal==ara[i]){
+            araEqual++;
+        }else{continue;}
+    }
 
-
-    
+    if(araEqual==N){
+        printf("YES");
+    }else{printf("NO");}
     return 0;
 }
